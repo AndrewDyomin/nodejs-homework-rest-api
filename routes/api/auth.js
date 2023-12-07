eeebc84189142a29c26409c34628bf616591891c
AndrewDyomin <dyomin.andrew1@gmail.com>
2023-12-08 01:26:29 +0200

    HW-05

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -7,6 +7,7 @@ const router = express.Router();
 const jsonParser = express.json();
 
 router.post("/register", jsonParser, AuthController.register);
+router.get("/register/verify/:verificationToken", jsonParser, AuthController.verifyUser);
 router.post("/login", jsonParser, AuthController.login)
 router.post("/logout", jsonParser, isAuth, AuthController.logout)
 router.post("/current", jsonParser, isAuth, AuthController.current)
